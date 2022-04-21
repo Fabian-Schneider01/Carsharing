585e832198717847d55d33c76d1c056b159ae56f
Fabian Schneider <inf20182@lehre.dhbw-stuttgart.de>
2022-04-21 21:32:18 +0200

    feat(profile + finder): second concept
    basic elements of finder included and modals for profile page created

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import *
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def home():
     return render_template("index.html")
@@ -14,9 +13,14 @@ def login():
 @app.route('/register')
 def register():
     return render_template("register.html")
+
 @app.route('/profile')
 def profile():
     return render_template('profile.html')
+
+@app.route('/findCar')
+def findCar():
+    return render_template('findCar.html')
 
 if __name__ == '__main__':
     app.run()
