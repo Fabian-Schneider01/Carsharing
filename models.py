1ad1154974d8ea8519fbd206278adf6ada94d615
Fabian Schneider <inf20182@lehre.dhbw-stuttgart.de>
2022-04-25 11:57:03 +0200

    feat(tables): second insert

--- models.py
+++ models.py
@@ -3,9 +3,9 @@ import sqlite3
 con = sqlite3.connect('database.sqlite')
 
 cur = con.cursor()
-"""
+
 cur.execute('''
-    CREATE TABLE Adresse(
+    CREATE TABLE IF NOT EXISTS Adresse(
     AdressID INTEGER NOT NULL PRIMARY KEY, 
     Straße TEXT, 
     Hausnummer INT, 
@@ -13,10 +13,10 @@ cur.execute('''
     Postleitzahl TEXT
     )
 ''')
-"""
+
 
 cur.execute('''
-    CREATE TABLE User(
+    CREATE TABLE IF NOT EXISTS User(
     UserID INTEGER NOT NULL PRIMARY KEY,
     Benutzername TEXT, 
     Vorname TEXT, 
@@ -30,7 +30,7 @@ cur.execute('''
 ''')
 
 cur.execute('''
-    CREATE TABLE Autos(
+    CREATE TABLE IF NOT EXISTS Autos(
     AutoID INT INTEGER NOT NULL PRIMARY KEY, 
     Hersteller TEXT, 
     Modell TEXT, 
@@ -42,7 +42,7 @@ cur.execute('''
 ''')
 
 cur.execute('''
-    CREATE TABLE Mietauftrag(
+    CREATE TABLE IF NOT EXISTS Mietauftrag(
     Mieter INT,
     Vermieter INT,
     Auto INT,
@@ -57,7 +57,7 @@ cur.execute('''
 ''')
 
 cur.execute('''
-    CREATE TABLE Autobesitzer(
+    CREATE TABLE IF NOT EXISTS Autobesitzer(
     User INT,
     Auto INT,
     FOREIGN KEY(User) REFERENCES User(UserID),
@@ -70,7 +70,17 @@ cur.execute('''
 ''')
 
 cur.execute('''
+    INSERT INTO Autos VALUES(123, "Mercedes", "EQS", "Kombi", 50, "2022-04-24", "2022-04-27")
+''')
+
+cur.execute('''
     SELECT * FROM Adresse
+''')
+
+print(cur.fetchall())
+
+cur.execute('''
+    SELECT * FROM Autos
 ''')
 
 print(cur.fetchall())
