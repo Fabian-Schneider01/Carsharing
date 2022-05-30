41e5fb0773063fe248922c4993ed39ea985e1c46
Fabian Schneider <inf20182@lehre.dhbw-stuttgart.de>
2022-05-30 21:46:26 +0200

    added comments

--- app.py
+++ app.py
@@ -15,39 +15,50 @@ with app.app_context():
 def home():
     return render_template("login.html")
 
+#login seite
 @app.route("/login", methods = ["GET", "POST"])
 def login():
     userFound = None
+    #post methode für login button
     if request.method == "POST":
         email = request.form["email"]
         password = request.form["password"]
         with sqlite3.connect("database.sqlite") as con:
             cur = con.cursor()
+            #in datenbank nachschauen ob email und password übereinstimmt
             cur.execute("SELECT UserID from User WHERE Email=(?) AND Passwort=(?)", [(email), (password)])
             input = cur.fetchall()
+            #wenn input gegeben und login button aktiv
             if len(input) > 0:
                 print("login Success")
                 userFound = 1
                 session["UserID"] = cur.execute("SELECT UserID from User WHERE Email=(?)", [(email)]).fetchone()[0]
                 return redirect(url_for("profile"))
+            #user nicht gefunden
             else: 
                 print("error: user not found")
                 userFound = 0
                 
     return render_template("login.html", userFound = userFound)
 
+#logout funktion
 @app.route("/logout", methods=["POST", "GET"])
 def logout():
+    #session beenden
     session.pop("UserID", None)
+    #zurück zur login seite leiten, nach logout
     return redirect(url_for("login"))
 
+#registrierungsseite
 @app.route("/register", methods = ["GET", "POST"])
 def register():
     emptyField = None
     emailExists = None
     usernameExists = None
     pwNotMatching = None
+    #registrierungsbutton aktiv
     if request.method == "POST":
+        #benötigte Felder
         username = request.form["username"]
         firstName = request.form["firstName"]
         lastName = request.form["lastName"]
@@ -58,10 +69,12 @@ def register():
         postalCode = request.form["postalCode"]
         password = request.form["password"]
         passwordConfirm = request.form["passwordConfirm"]
+        #error, wenn nicht alle felder ausgefüllt wurden
         if username == "" or firstName == "" or lastName == "" or email == "" or street == "" or houseNum == "" or city == "" or postalCode == "" or password == "" or passwordConfirm== "":
             print("empty field")
             emptyField = 1
         else:
+            #alle Felder ausgefüllt
             emptyField = 0
             with sqlite3.connect("database.sqlite") as con:
                 cur = con.cursor()
@@ -69,15 +82,19 @@ def register():
                 findEmail = cur.fetchall()
                 cur.execute("SELECT Benutzername FROM User WHERE Benutzername=(?)", [(username)])
                 findUsername = cur.fetchall()
+                #überprüfen ob email bereits existiert
                 if len(findEmail) > 0:
                     print("email already exists")
                     emailExists = 1
+                #überprüfen ob username existiert
                 if len(findUsername) > 0:
                     print("username already exists")
                     usernameExists = 1
+                #überprüfen ob passwörter übereinstimmen
                 if password != passwordConfirm:
                     print("password not matching")
                     pwNotMatching = 1
+                #lege user an, wenn kein error auftritt
                 if emailExists != 1 and usernameExists != 1 and pwNotMatching != 1:
                     cur.execute("INSERT INTO Adresse(Straße, Hausnummer, Ort, Postleitzahl) VALUES(?, ?, ?, ?)", (street, houseNum, city, postalCode))
                     cur.execute("SELECT * FROM Adresse ORDER BY AdressID DESC LIMIT 1")
@@ -85,10 +102,12 @@ def register():
                     cur.execute("INSERT INTO User(Benutzername, Vorname, Nachname, Email, Passwort, Guthaben, Adresse) VALUES(?, ?, ?, ?, ?, 0, ?)", (username, firstName, lastName, email, password, addressID))
                     con.commit()
                     print("registration successful")
+                    #nach erfolgreicher registrierung zur login seite leiten
                     return redirect(url_for("login"))
 
     return render_template("register.html", emptyField = emptyField, emailExists = emailExists, usernameExists = usernameExists, pwNotMatching = pwNotMatching)
 
+#profil seite
 @app.route("/profile", methods = ["GET", "POST"])
 def profile():
     email = None
@@ -104,6 +123,7 @@ def profile():
     password = None
     rented = []
     periods = []
+    #wenn nicht eingeloggt, zurück zur login
     if request.method == "GET":
         if session.get("UserID") is None:
             with sqlite3.connect("database.sqlite") as con:
@@ -112,6 +132,7 @@ def profile():
             return redirect(url_for("login"))
         else:
             with sqlite3.connect("database.sqlite") as con:
+                #profilansicht mit userdaten füllen
                 cur = con.cursor()
                 userID = session["UserID"]
                 email = cur.execute("SELECT Email from User WHERE UserID=(?)", [(userID)]).fetchone()[0]
@@ -125,11 +146,10 @@ def profile():
                 city = cur.execute("SELECT Ort from Adresse WHERE AdressID=(?)", [(addressID)]).fetchone()[0]
                 postalCode = cur.execute("SELECT Postleitzahl from Adresse WHERE AdressID=(?)", [(addressID)]).fetchone()[0]
                 credit = cur.execute("SELECT Guthaben from User WHERE UserID=(?)", [(userID)]).fetchone()[0]
-                #periods = cur.execute("SELECT Datum FROM Verfuegbar WHERE Auto=(?)", [()])
                 print(periods)
             con.commit()
             with sqlite3.connect("database.sqlite") as con:
-            # for displaying all the car the user has added
+                #zeige alle Fahrzeuge in eigene Autos und gemietete Autos an
                 cur = con.cursor()
                 userID = session["UserID"]
                 cars = cur.execute("SELECT AutoID, Hersteller, Modell, Fahrzeugtyp, PreisProTag FROM User LEFT JOIN Autobesitzer ON User.UserID = Autobesitzer.User LEFT JOIN Autos ON Autobesitzer.Auto = Autos.AutoID WHERE UserID=(?)", [(userID)]).fetchall()
@@ -140,7 +160,7 @@ def profile():
                     all_dates.append(dates)
                     rented = cur.execute("SELECT Startdatum, Enddatum FROM Mietauftrag WHERE Auto=(?)", [(car[0])]).fetchall()
                     all_rented.append(rented)
-
+    #profil verändern ansicht
     if request.method == "POST" and request.form['formButton'] == "Speichern":
         username = request.form["username"]
         email = request.form["email"]
@@ -153,6 +173,7 @@ def profile():
         credit = request.form["credit"]
         with sqlite3.connect("database.sqlite") as con:
             cur = con.cursor()
+            #überprüfen ob alle felder ausgefüllt sind, bevor ein update der daten stattfindet
             if username!="":
                 cur.execute("UPDATE User SET Benutzername=(?) WHERE UserID=(?)", [username, session["UserID"]])
             if email!="":
@@ -173,8 +194,9 @@ def profile():
             cur.fetchall()
         con.commit()
         return redirect(url_for("profile"))
-
+    #button um eigene autos hinzuzufügen
     elif request.method == 'POST' and request.form['formButton'] == "Hinzufügen":
+        #nach modell, fahrzeugtyp, hersteller und preis abfragen => bezogen auf user session
         modell = request.form['modell']
         fahrzeugtyp = request.form['fahrzeugtyp']
         hersteller = request.form['hersteller']
@@ -190,6 +212,7 @@ def profile():
     print(cars[0][0])
     return render_template("profile.html", email = email, username = username, firstName = firstName, lastName = lastName, street = street, houseNum = houseNum, city = city, postalCode = postalCode, credit = credit, password = password, cars = cars, rentCars = [], rented = rented, periods = periods, all_dates = all_dates, all_rented = all_rented)
 
+#methoden um autos zu verändern
 @app.route("/edit-car/<id>", methods=['GET', 'POST'])
 def edit_car(id):
     modell = request.form['modell']
@@ -201,8 +224,6 @@ def edit_car(id):
             cur = con.cursor()
             cur.execute("UPDATE Autos SET Hersteller=(?), Modell=(?), Fahrzeugtyp=(?), PreisProTag=(?) WHERE AutoID=(?)", [(hersteller), (modell), (fahrzeugtyp), (preis), (id)])
     con.commit()
-    # TODO
-    # fix the default dates and price in profile.html
     return redirect(url_for("profile"))
 
 @app.route("/add-timeframe/<id>", methods=['GET', 'POST'])
@@ -265,27 +286,9 @@ def findCar():
                 cur = con.cursor()
                 userID = session["UserID"]
                 cars = cur.execute("SELECT AutoID, Hersteller, Modell, Fahrzeugtyp, PreisProTag FROM Autos").fetchall()
-                """
-                # rename cars in the above line to cardata
-                cardates = cur.execute("SELECT Datum FROM Verfuegbar WHERE Auto=(?)", [(id)]).fetchall()[0]
-                cardate = None
-                while cardate == None:
-                    for date in cardates:   # find first candidate in the future
-                        if (cardate - date.today) > 0:
-                            cardate = date
-                            break
-                if cardate == None:
-                    ##error! nicht verfügbar, dieses car nicht anzeigen
-                for date in cardates:
-                    if (date - date.today)>0 and (date-date.today)<(cardate-date.today)
 
-                cars = (cardata[0], cardata[1], cardata[2], cardata[3], cardata[4], cardate)    # TODO check if new Structure is used everywhere, so cars[5] is date and cars[6] nonexistent
-                """
         if request.method == "POST":
-            #change name to rentCar-i => i = AutoID 
             with sqlite3.connect("database.sqlite") as con:
-                #available = cur.execute("SELECT Datum FROM Verfuegbar WHERE Auto=(?)", [id]).fetchall()[0]
-                #available_dates = [datetime.date.fromisoformat(i) for i in available]
 
                 cur = con.cursor()
                 filterPlace = request.form["place"]
@@ -295,12 +298,14 @@ def findCar():
                     filterEnddate = request.form["enddate"]
                     filterClass = request.form["carclass"]
                     filterMaxPrice = request.form["maxprice"]
-                    #cars = cur.execute(filter(filterPlace, filterStartdate, filterEnddate, filterClass, filterMaxPrice))
-                    
-                    #cur.execute("SELECT UserID FROM User INNER JOIN Adresse ON Adresse.AdressID = User.Adresse WHERE Adresse.Ort == (?)", [(filterPlace)]).fetchall() 
-                    
+                    incompleteFilter = 0
+                    if filterPlace == "" and filterStartdate == "" and filterEnddate == "" and filterClass == "Fahrzeugklasse" and filterMaxPrice == "":
+                        print("Bitte alle Felder ausfüllen")
+                        incompleteFilter = 1
+                    else:
+                        incompleteFilter = 0
+
                     print("Filtered Search: ", filterPlace, " ", filterStartdate, " ", filterEnddate, " ", filterClass, " ", filterMaxPrice)
-                    #for i in range(cur.execute("SELECT COUNT(*) FROM Autos").fetchone()[0]):
                     matchingUser = cur.execute("SELECT UserID FROM User INNER JOIN Adresse ON Adresse.AdressID = User.Adresse WHERE Adresse.Ort == (?)", [(filterPlace)]).fetchall()              
                     if matchingUser != []:
                         print(matchingUser[0][0])
@@ -311,30 +316,8 @@ def findCar():
                                 if filterClass in matchedUserCars[i][3]:
                                     if matchedUserCars != []:
                                         return render_template("findCar.html", cars = matchedUserCars)
-                                    """if filterStartdate >= matchedUserCars[i][5] and filterStartdate <= matchedUserCars[i][6] and filterEnddate >= matchedUserCars[i][5] and filterEnddate <= matchedUserCars[i][6]:
-                                        totalPrice = (datetime.strptime(filterEnddate, "%Y-%m-%d") - datetime.strptime(filterStartdate, "%Y-%m-%d")).days * matchedUserCars[i][4]
-                                        if totalPrice <= int(filterMaxPrice):
-                                            cars = matchedUserCars
-                                            session["matchedCars"] = matchedUserCars
-                                            return render_template("findCar.html", cars = cars) """ 
                         
-                                    
                     print("Filtered Search: ", filterPlace, " ", filterStartdate, " ", filterEnddate, " ", filterClass, " ", filterMaxPrice)
-                    #for i in range(cur.execute("SELECT COUNT(*) FROM Autos").fetchone()[0]):
-                    """matchingUser = cur.execute("SELECT UserID FROM User INNER JOIN Adresse ON Adresse.AdressID = User.Adresse WHERE Adresse.Ort == (?)", [(filterPlace)]).fetchall()              
-                    if matchingUser != []:
-                        print(matchingUser[0][0])
-                        matchedUserCars = cur.execute("SELECT AutoID, Hersteller, Modell, Fahrzeugtyp, PreisProTag, Startdatum, Enddatum FROM User LEFT JOIN Autobesitzer ON User.UserID = Autobesitzer.User LEFT JOIN Autos ON Autobesitzer.Auto = Autos.AutoID WHERE UserID=(?)", [(matchingUser[0][0])]).fetchall()
-                        print(matchedUserCars[0])
-                        if matchedUserCars != "":
-                            for i in range(len(matchedUserCars)):
-                                if filterClass in matchedUserCars[i][3]:
-                                    if filterStartdate >= matchedUserCars[i][5] and filterStartdate <= matchedUserCars[i][6] and filterEnddate >= matchedUserCars[i][5] and filterEnddate <= matchedUserCars[i][6]:
-                                        totalPrice = (datetime.strptime(filterEnddate, "%Y-%m-%d") - datetime.strptime(filterStartdate, "%Y-%m-%d")).days * matchedUserCars[i][4]
-                                        if totalPrice <= int(filterMaxPrice):
-                                            cars = matchedUserCars
-                                            session["matchedCars"] = matchedUserCars
-                                            return render_template("findCar.html", cars = cars)"""
                 else:
                     matchedCars = cur.execute("SELECT AutoID, Hersteller, Modell, Fahrzeugtyp, PreisProTag from Autos LEFT JOIN Autobesitzer").fetchall()[0]  # where autobesitzer ungleich session user. TODO Autobesitzer tabelle killen
                     for i in range(len(matchedCars)):
@@ -387,29 +370,14 @@ def findCar():
                             
                             break
                     
-    """            
-    return render_template("findCar.html", cars = cars)#producer=producer, model=model, price=price)
-            # for displaying all the cars the user has added
-            cur = con.cursor()
-            cars = cur.execute("SELECT AutoID, Hersteller, Modell, Fahrzeugtyp, PreisProTag, Startdatum, Enddatum FROM Autos").fetchall()
-            # TODO: dont select the cars where the current user is the owner
-            # TODO: call the table with all the timeframes where the cars have already been rented for the detail view
-            # TODO: join with the owners' table
-    # is called when the user clicked to rent the car
-    """
     if request.method == 'POST':
-        # TODO hier kommt rent car redirect hin!! 
         startdate = request.form['startdate']
         print(startdate)
         enddate = request.form['enddate']
         print(enddate)
         current_user_id = session['UserID']
         id_car = None
-        # TODO: insert car renting order details into database.
-        #with sqlite3.connect("database.sqlite") as con:
-        #    cur.execute("INSERT INTO Autobesitzer VALUES((?), (?))", [current_user_id, car_id])
-        #con.commit()
-    return render_template("findCar.html", cars = cars)
+    return render_template("findCar.html", cars = cars, incompleteFilter = incompleteFilter)
 
 @app.route("/rent-car/<id>", methods=['GET', 'POST'])
 def rent_car(id):
@@ -428,8 +396,6 @@ def rent_car(id):
             allRentedDates = []
             date = startdate
             while date <= enddate:
-                #available = cur.execute("SELECT Datum FROM Verfuegbar WHERE Auto=(?) and Datum=(?)", [(id), (date)]).fetchone()
-                #available = cur.execute("SELECT * FROM Verfuegbar").fetchall()   # TUT NICHT
                 available = cur.execute("SELECT Datum FROM Verfuegbar WHERE Auto=(?) and Datum=(?)", [(id), (date)]).fetchone()
                 if available == None:
                     raise Exception("Der angeforderte Zeitraum ist nicht verfügbar!")
